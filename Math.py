ab7a3098bf35a720614f547b2adb73bd6e82aede
DuPiMan <bryton@themoellers.net>
2016-01-16 15:28:15 -0800

    first bit of calculations

--- Math.py
+++ Math.py
@@ -24,11 +24,22 @@ class Calculator(object):
 				timds.append(timd)
 		return timds
 
+	def getPlayedTIMDsForTeamNumber(self, num):
+		timds = []
+		for timd in self.comp.TIMDs:
+			if timd.teamNumber == num and timd.numLowShotsMissedAuto != -1:
+				timds.append(timd)
+		return timds
+
 	# Calculated Team Data
 	def averageTIMDObjectOverMatches(self, teamNumber, key, coefficient):
-		timds = self.getTIMDsForTeamNumber(teamNumber)
+		timds = self.getPlayedTIMDsForTeamNumber(teamNumber)
+		if len(timds) == 0:
+			print "No played TIMDs for " + teamNumber
+			return -1
 		total = 0.0
 		for timd in timds:
-			print utils.makeDictFromTIMD(timd)[key]
+			 total = total + utils.makeDictFromTIMD(timd)[key]
+		return total/len(timds)
 
 
